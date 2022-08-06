e20071cd011f856403cb6f1edbb29a25976937d1
windo-developer <yui050505@gmail.com>
2022-08-06 01:38:14 -0700

    ui2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,52 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 px-10 py-20 grid gap-10">
-      <div className='bg-white p-10 rounded-3xl shadow-xl'>
-      <span className='font-bold text-3xl'>Select Item</span>
-      <div className='flex justify-between my-2'>
-        <span className='text-gray-500'>Grey Chair</span>
-        <span className='font-semibold'>$19</span>
+    <div className="bg-slate-400 px-20 py-20 grid gap-10">
+      <div className="bg-white p-10 rounded-3xl shadow-xl">
+        <span className="font-bold text-3xl">Select Item</span>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-gray-500">Grey Table</span>
+          <span className="font-semibold">$190</span>
+        </div>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span className="text-xl">Total</span>
+          <span className="font-semibold">$209</span>
+        </div>
+        <div className="mt-5 bg-blue-500 text-white p-2 text-center rounded-xl w-2/4 mx-auto">
+          Check Out
+        </div>
       </div>
-      <div className='flex justify-between'>
-        <span className='text-gray-500'>Grey Table</span>
-        <span className='font-semibold'>$190</span>
+      <div className="bg-white overflow-hidden rounded-2xl shadow-xl">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+        <div className="rounded-3xl p-6 bg-white relative -top-5">
+          <div className="flex relative -top-16 items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="h-24 w-24 bg-red-400 rounded-full"></div>
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$2,310</span>
+            </div>
+          </div>
+          <div className="flex relative flex-col -mt-12 -mb-5 items-center">
+            <span className="text-lg font-medium">JaeWon</span>
+            <span className="text-sm text-gray-500">Korea, Seoul</span>
+          </div>
+        </div>
       </div>
-      <div className='flex justify-between mt-2 pt-2 border-t-2 border-dashed'>
-        <span className='text-xl'>Total</span>
-        <span className='font-semibold'>$209</span>
-      </div>
-      <div className='mt-5 bg-blue-500 text-white p-2 text-center rounded-xl w-2/4 mx-auto'>Check Out</div>
-      </div>
-      <div className='bg-white p-10 rounded-2xl shadow-xl'></div>
-      <div className='bg-white p-10 rounded-2xl shadow-xl'></div>
-      <div className='bg-white p-10 rounded-2xl shadow-xl'></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
